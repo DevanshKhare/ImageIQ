440d51e5a5668064749450fbfbdf9b8e751e5bbd
Devansh Khare <devanshkhare0@gmail.com>
2024-03-30 23:08:12 +0530

    image model created

---
+++ lib/database/models/image.model.ts
@@ -0,0 +1,54 @@
+import { Schema, model, models } from "mongoose";
+
+const ImageSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  transformationType: {
+    type: String,
+    required: true,
+  },
+  publicId: {
+    type: String,
+    required: true,
+  },
+  secureUrl: {
+    type: URL,
+    required: true,
+  },
+  width: {
+    type: Number,
+  },
+  height: {
+    type: Number,
+  },
+  config: {
+    type: Object,
+  },
+  transformationUrl: {
+    type: URL,
+  },
+  aspectRatio: {
+    type: String,
+  },
+  color: {
+    type: String,
+  },
+  prompt: {
+    type: String,
+  },
+  author: {
+    type: String,
+  },
+  createdAt: {
+    type: String,
+  },
+  updatedAt: {
+    type: String,
+  },
+});
+
+const Image = models?.Image || model("Image", ImageSchema);
+
+export default Image;
