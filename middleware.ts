2fc596bc9940d33bf821f8b9e50cf77337623d5c
DevanshKhare <69037398+DevanshKhare@users.noreply.github.com>
2024-04-05 18:08:30 +0530

    removed unwanted code

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,5 @@
 import { authMiddleware } from "@clerk/nextjs";
  
-
 export default authMiddleware({
   // Allow signed out users to access the specified routes:
   // publicRoutes: ['/anyone-can-visit-this-route'],
