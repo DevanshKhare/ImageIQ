76b8a9a0394b6104b09d163d57cb454b56d64bfe
DevanshKhare <69037398+DevanshKhare@users.noreply.github.com>
2024-04-06 06:56:28 +0530

    created user model

--- lib/database/models/user.mode.ts
+++ lib/database/models/user.mode.ts
@@ -1,5 +1,16 @@
 import { Schema, model, models } from "mongoose";
 
+export interface UserI extends Document{
+  clerkId: string;
+  email: string;
+  username: string;
+  photo: string;
+  firstName: string;
+  lastName: string;
+  planId: string;
+  creditBalance: number;
+}
+
 const UserSchema = new Schema({
   clerkId: {
     type: String,
