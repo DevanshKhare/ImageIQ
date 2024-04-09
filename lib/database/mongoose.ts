1830367513b831621e0dc66143064573b0747367
Devansh Khare <devanshkhare0@gmail.com>
2024-04-09 13:28:02 +0530

    user actions checkin

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -16,7 +16,7 @@ if (!cached) {
   };
 }
 
-export const connectToDB = async () => {
+export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
 
   if (!MONGODB_URL) throw new Error("Missing MONGODB_URL")
