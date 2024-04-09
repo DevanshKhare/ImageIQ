c03dc6ca56dcfbf0e4c9d55bea9ec84e612ffbc3
Devansh Khare <devanshkhare0@gmail.com>
2024-04-09 21:21:35 +0530

    completed media uploader

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -1,8 +1,11 @@
 "use client";
+
 import React from "react";
 import { useToast } from "../ui/use-toast";
-import { CldUploadWidget } from "next-cloudinary";
+import { CldImage, CldUploadWidget } from "next-cloudinary";
 import Image from "next/image";
+import { dataUrl, getImageSize } from "@/lib/utils";
+import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
 
 type MediaUploaderProps = {
   onValueChange: (value: string) => void;
@@ -21,6 +24,16 @@ const MediaUploader = ({
   const { toast } = useToast();
 
   const onUploadSuccessHandler = (result: any) => {
+    setImage((prev:any)=> ({
+        ...prev,
+        publicId: result?.info?.public_id,
+        width: result?.info?.width,
+        height: result?.info?.height,
+        secureUrl: result?.info?.secure_url
+    }))
+
+    onValueChange(result?.info?.public_id)
+
     toast({
       title: "Image uploaded successfully",
       description: "You used 1 of your credit balance",
@@ -50,17 +63,33 @@ const MediaUploader = ({
       {({ open }) => (
         <div className="flex flex-col gap-4">
           <h3 className="h3-bold text-dark-600">Original</h3>
-          {publicId ? <>Here is the image</> : <div className="media-uploader_cta" onClick={()=>open()}>
-            
-            <div className="media-uploader_cta-image">
-                <Image src="/assets/icons/add.svg" alt="Add Image"
-                width={24}
-                height={24}
-
+          {publicId ? (
+            <>
+              <div className="cursor-pointer overflow-hidden rounded-[10px]">
+                <CldImage
+                  width={getImageSize(type, image, "width")}
+                  height={getImageSize(type, image, "height")}
+                  src={publicId}
+                  alt="image"
+                  sizes={"(max-width:767px) 100vw, 50vw"}
+                  placeholder={dataUrl as PlaceholderValue}
+                  className="media-uploader_cldImage"
                 />
+              </div>
+            </>
+          ) : (
+            <div className="media-uploader_cta" onClick={() => open()}>
+              <div className="media-uploader_cta-image">
+                <Image
+                  src="/assets/icons/add.svg"
+                  alt="Add Image"
+                  width={24}
+                  height={24}
+                />
+              </div>
+              <p className="p-14-medium">Click here to upload image</p>
             </div>
-                <p className="p-14-medium">Click here to upload image</p>
-            </div>}
+          )}
         </div>
       )}
     </CldUploadWidget>
