5bc5378bf17aa33af2df86416ccce47b3e99b3e0
Devansh Khare <devanshkhare0@gmail.com>
2024-04-10 22:43:41 +0530

    completed image details page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,17 @@
+import { Collection } from "@/components/shared/Collection";
 import { navLinks } from "@/constants";
+import { getAllImages } from "@/lib/actions/image.actions";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Page = () => {
+const Page = async({ searchParams}: SearchParamProps) => {
+  const page = Number(searchParams?.page)||1;
+  const searchQuery = (searchParams?.query as string) || "";
+
+  const images = await getAllImages({
+    page, searchQuery
+  })
   return (
     <>
       <section className="home">
@@ -25,9 +33,17 @@ const Page = () => {
           ))}
         </ul>
       </section>
+      <section className="sm:mt-12">
+          <Collection 
+            hasSearch={true}
+            images={images?.data}
+            totalPages={images?.totalPage}
+            page={page}
+          />
+      </section>
     </>
   );
-};
+}; 
 
 
 export default Page;
--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -1,76 +1,139 @@
-"use server"
+"use server";
 
 import { revalidatePath } from "next/cache";
-import { connectToDatabase } from "../database/mongoose"
-import { handleError } from "../utils"
+import { connectToDatabase } from "../database/mongoose";
+import { handleError } from "../utils";
 import User from "../database/models/user.model";
 import Image from "../database/models/image.model";
 import { redirect } from "next/navigation";
-
-const populateUser = (query:any) => query.populate({
+import { v2 as cloudinary } from "cloudinary";
+const populateUser = (query: any) =>
+  query.populate({
     path: "author",
     model: User,
     select: "_id firstName lastName",
-})
+  });
 
-export async function addImage({image, userId, path}: AddImageParams){
-    try {
-        await connectToDatabase();
-        const author = await User.findById(userId);
-        
-        if(!author) {
-            throw new Error("User not found");
-        }
+export async function addImage({ image, userId, path }: AddImageParams) {
+  try {
+    await connectToDatabase();
+    const author = await User.findById(userId);
 
-        const newImage = await Image.create({
-            ...image,
-            author: author._id
-        })
-        revalidatePath(path);
-        return JSON.parse(JSON.stringify(newImage))
-    } catch (error) {
-        handleError(error)
+    if (!author) {
+      throw new Error("User not found");
     }
+
+    const newImage = await Image.create({
+      ...image,
+      author: author._id,
+    });
+    revalidatePath(path);
+    return JSON.parse(JSON.stringify(newImage));
+  } catch (error) {
+    handleError(error);
+  }
 }
 
-export async function updateImage({image, userId, path}: UpdateImageParams){
-    try {
-        await connectToDatabase();
-        
-        const imageToUpdate = await Image.findById(image._id)
+export async function updateImage({ image, userId, path }: UpdateImageParams) {
+  try {
+    await connectToDatabase();
 
-        if(!imageToUpdate || imageToUpdate.author.toHexString() !== userId){
-            throw new Error("Unauthorized or image not found")
-        }
+    const imageToUpdate = await Image.findById(image._id);
 
-        const updateImage = await Image.findByIdAndUpdate(imageToUpdate._id, image, {new: true})
-
-        revalidatePath(path);
-        return JSON.parse(JSON.stringify(image))
-    } catch (error) {
-        handleError(error)
+    if (!imageToUpdate || imageToUpdate.author.toHexString() !== userId) {
+      throw new Error("Unauthorized or image not found");
     }
+
+    const updateImage = await Image.findByIdAndUpdate(
+      imageToUpdate._id,
+      image,
+      { new: true }
+    );
+
+    revalidatePath(path);
+    return JSON.parse(JSON.stringify(image));
+  } catch (error) {
+    handleError(error);
+  }
 }
 
-export async function deleteImage(imageId: string){
-    try {
-        await connectToDatabase();
-        await Image.findByIdAndDelete(imageId)
-    } catch (error) {
-        handleError(error)
-    }
-    finally {
-        redirect("/")
-    }
+export async function deleteImage(imageId: string) {
+  try {
+    await connectToDatabase();
+    await Image.findByIdAndDelete(imageId);
+  } catch (error) {
+    handleError(error);
+  } finally {
+    redirect("/");
+  }
 }
 
-export async function getImageById(imageId: string){
-    try {
-        await connectToDatabase();
-        const image = await populateUser(Image.findById(imageId))
-        if(!image) throw new Error("Image not found")
-        return JSON.parse(JSON.stringify(image))
-    } catch (error) {
-        handleError(error)
+export async function getImageById(imageId: string) {
+  try {
+    await connectToDatabase();
+    const image = await populateUser(Image.findById(imageId));
+    if (!image) throw new Error("Image not found");
+    return JSON.parse(JSON.stringify(image));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+export async function getAllImages({
+  limit = 9,
+  page = 1,
+  searchQuery = "",
+}: {
+  limit?: number;
+  page: number;
+  searchQuery?: string;
+}) {
+  try {
+    await connectToDatabase();
+
+    cloudinary.config({
+      cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+      secure: true,
+    });
+
+    let expression = "folder=imageiq";
+
+    if (searchQuery) {
+      expression += ` AND ${searchQuery}`;
     }
+
+    const { resources } = await cloudinary.search
+      .expression(expression)
+      .execute();
+
+    const resourceIds = resources.map((resource: any) => resource.public_id);
+
+    let query = {};
+    if (searchQuery) {
+      query = {
+        publicId: {
+          $in: resourceIds,
+        },
+      };
+    }
+
+    const skipAmout = (Number(page) - 1) * limit;
+    const images = await populateUser(Image.find(query))
+      .sort({ updatedAt: -1 })
+      .skip(skipAmout)
+      .limit(limit);
+
+    const totalImages = await Image.find(query).countDocuments();
+    const savedImages = await Image.find().countDocuments();
+
+    return {
+      data: JSON.parse(JSON.stringify(images)),
+      totalPage: Math.ceil(totalImages/limit),
+      savedImages
+    }
+  } catch (error) {
+    handleError(error);
+  }
 }
