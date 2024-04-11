47034cb1022f0d7668b0ea06e55b4d5db92161ed
Devansh Khare <devanshkhare0@gmail.com>
2024-04-11 08:03:03 +0530

    fixed middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: ['/api/webhooks/clerk'],
+  publicRoutes: ['/','/api/webhooks/clerk','/api/webhooks/stripe'],
   // Routes that can always be accessed, and have
   // no authentication information
-  ignoredRoutes: ['/api/webhooks/clerk'],
 });
  
 export const config = {
