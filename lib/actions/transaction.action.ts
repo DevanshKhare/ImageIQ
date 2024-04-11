953ddcdd57b1b43137317d71113968d1294ed9a9
Devansh Khare <devanshkhare0@gmail.com>
2024-04-11 07:41:00 +0530

    transaction model

--- lib/actions/transaction.action.ts
+++ lib/actions/transaction.action.ts
@@ -23,7 +23,7 @@ export async function checkoutCredits(transaction: any) {
           },
         },
         quantity: 1,
-      },
+      }
     ],
     metadata: {
       plan: transaction.plan,
@@ -32,7 +32,7 @@ export async function checkoutCredits(transaction: any) {
     },
     mode: 'payment',
     success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
-    cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}`,
+    cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
   });
 
   redirect(session.url!);
